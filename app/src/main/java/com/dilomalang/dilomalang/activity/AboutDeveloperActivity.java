a2b6ed703390899747a0ad07db4f6802bf447423
fahruddin <yusufudin14431@gmail.com>
2016-06-25 00:38:34 +0700

    update about_developer ditunggu review nya

--- app/src/main/java/com/dilomalang/dilomalang/activity/AboutDeveloperActivity.java
+++ app/src/main/java/com/dilomalang/dilomalang/activity/AboutDeveloperActivity.java
@@ -1,6 +1,14 @@
 package com.dilomalang.dilomalang.activity;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.dilomalang.dilomalang.R;
+import com.dilomalang.dilomalang.adapter.DeveloperAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created on : June/23/2016
@@ -14,4 +22,25 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class AboutDeveloperActivity extends AppCompatActivity {
+
+    ListView lv;
+    Context context;
+
+    ArrayList prgmName;
+    public static int [] prgmImages={R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample};
+    public static String [] prgmNameList={"Nama 1","Nama 2","Nama 3","Nama 4","Nama 5","Nama 6","Nama 7","Nama 8","Nama 9","Nama 10"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dilo_page_about_dev);
+
+        context=this;
+
+        lv=(ListView) findViewById(R.id.listView);
+        lv.setAdapter(new DeveloperAdapter(this, prgmNameList,prgmImages));
+
+    }
+
+
+
 }
--- app/src/main/java/com/dilomalang/dilomalang/adapter/DeveloperAdapter.java
+++ app/src/main/java/com/dilomalang/dilomalang/adapter/DeveloperAdapter.java
@@ -1,8 +1,18 @@
 package com.dilomalang.dilomalang.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.dilomalang.dilomalang.R;
+import com.dilomalang.dilomalang.activity.AboutDeveloperActivity;
 import com.dilomalang.dilomalang.item.Developer;
 
 /**
@@ -20,11 +30,62 @@ import com.dilomalang.dilomalang.item.Developer;
  * Kelas ini untuk developer list adapter
  */
 
-public class DeveloperAdapter extends ArrayAdapter<Developer> {
+public class DeveloperAdapter extends BaseAdapter {
 
+    String [] result;
+    Context context;
+    int [] imageId;
+    private static LayoutInflater inflater=null;
 
-    public DeveloperAdapter(Context context, int resource, Developer[] objects) {
-        super(context, resource, objects);
+    public DeveloperAdapter(AboutDeveloperActivity mainActivity, String[] prgmNameList, int[] prgmImages) {
+        // TODO Auto-generated constructor stub
+        result=prgmNameList;
+        context=mainActivity;
+        imageId=prgmImages;
+        inflater = ( LayoutInflater )context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return result.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    public class Holder
+    {
+        TextView tv;
+        ImageView img;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        Holder holder=new Holder();
+        View rowView;
+        rowView = inflater.inflate(R.layout.dilo_page_item_list, null);
+        holder.tv=(TextView) rowView.findViewById(R.id.textView);
+        holder.img=(ImageView) rowView.findViewById(R.id.imageView);
+        holder.tv.setText(result[position]);
+        holder.img.setImageResource(imageId[position]);
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(context, "You Clicked "+result[position], Toast.LENGTH_LONG).show();
+            }
+        });
+        return rowView;
     }
 
 
