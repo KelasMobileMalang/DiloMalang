3668f73fe189515901f76d748d323d89df506158
fauzi <fauzialam5@gmail.com>
2016-06-26 02:22:39 +0700

    UI page Mainmenu

--- app/src/main/java/com/dilomalang/dilomalang/activity/MainMenuActivity.java
+++ app/src/main/java/com/dilomalang/dilomalang/activity/MainMenuActivity.java
@@ -1,6 +1,13 @@
 package com.dilomalang.dilomalang.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.dilomalang.dilomalang.R;
 
 /**
  * Created on : June/23/2016
@@ -14,4 +21,26 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dilo_page_mainmenu);
+
+
+
+        final LinearLayout button = (LinearLayout) findViewById(R.id.btnVisit);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://malang.dilo.id/"));
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
 }
