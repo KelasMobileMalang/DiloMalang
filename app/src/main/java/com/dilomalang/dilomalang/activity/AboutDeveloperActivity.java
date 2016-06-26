1296ebfacb29061a0cc2c80900fdbb7fe61dc76a
fahruddin <yusufudin14431@gmail.com>
2016-06-27 00:15:18 +0700

    update tampilan

--- app/src/main/java/com/dilomalang/dilomalang/activity/AboutDeveloperActivity.java
+++ app/src/main/java/com/dilomalang/dilomalang/activity/AboutDeveloperActivity.java
@@ -26,9 +26,14 @@ public class AboutDeveloperActivity extends AppCompatActivity {
     ListView lv;
     Context context;
 
-    public static int [] prgmImages={R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample};
-    public static String [] prgmNameList={"Nama 1","Nama 2","Nama 3","Nama 4","Nama 5","Nama 6","Nama 7","Nama 8","Nama 9","Nama 10"};
-    public static String [] emailList={"Email 1","Email 2","Email 3","Email 4","Email 5","Email 6","Email 7","Email 8","Email 9","Email 10"};
+    public static int [] prgmImages={R.drawable.ahmad_dzaki_afif_h,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.fauzi_firman_a,
+                                    R.drawable.m_fadlur_rahman,R.drawable.sample,R.drawable.sample,R.drawable.sample,R.drawable.sample};
+    public static String [] prgmNameList={"Ahmad Dzaki Afif H","Ahmad Roihan","Fadhli Al Mutawakkil","Fahruddin Yusuf H","Fauzi Firman A",
+                                        "M Fadlur Rahman","M Galang Arbi S","Syaiful Amri Yahya","Nama 9","Nama 10"};
+    public static String [] emailList={"dzakiafif12@gmail.com","ahmadroihanmlg@gmail.com","fadhli.tech@gmail.com","yusufudin14431@gmail.com","fauzialam5@gmail.com",
+                                        "fadlur2@live.com","Galangarbis@gmail.com","syaifulamriyahya@gmail.com","Email 9","Email 10"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/dilomalang/dilomalang/adapter/DeveloperAdapter.java
+++ app/src/main/java/com/dilomalang/dilomalang/adapter/DeveloperAdapter.java
@@ -2,6 +2,8 @@ package com.dilomalang.dilomalang.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,8 @@ import android.widget.Toast;
 import com.dilomalang.dilomalang.R;
 import com.dilomalang.dilomalang.activity.AboutDeveloperActivity;
 import com.dilomalang.dilomalang.item.Developer;
+
+import static android.widget.Toast.makeText;
 
 /**
  * Created on : June/23/2016
@@ -70,6 +74,15 @@ public class DeveloperAdapter extends BaseAdapter {
         TextView tv,email;
         ImageView img;
     }
+
+    public void pindahBrowser(String getLink){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse(getLink));
+        context.startActivity(intent);
+    }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         // TODO Auto-generated method stub
@@ -86,7 +99,38 @@ public class DeveloperAdapter extends BaseAdapter {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                Toast.makeText(context, "You Clicked "+result[position], Toast.LENGTH_LONG).show();
+
+                String[] link = new String[]{"https://github.com/dzakiafif", "https://github.com/ahmadroihan",
+                                            "https://github.com/eltawakkal/", "https://github.com/fahruddin14431",
+                                            "https://github.com/fauzi5896", "https://github.com/muhfadlurrahman",
+                                            "https://github.com/galangarbis", "https://github.com/sayvinzu",
+                                            "link9", "link10"};
+
+                if (position==0){
+                    pindahBrowser(link[0]);
+                }else if (position==1){
+                    pindahBrowser(link[1]);
+                }else if (position==2){
+                    pindahBrowser(link[2]);
+                }else if (position==3){
+                    pindahBrowser(link[3]);
+                }else if (position==4){
+                    pindahBrowser(link[4]);
+                }else if (position==5){
+                    pindahBrowser(link[5]);
+                }else if (position==6){
+                    pindahBrowser(link[6]);
+                }else if (position==7){
+                    pindahBrowser(link[7]);
+                }else if (position==8){
+                    pindahBrowser(link[8]);
+                }else if (position==9){
+                    pindahBrowser(link[9]);
+                }else if (position==10){
+                    pindahBrowser(link[10]);
+                }else{
+                    Toast.makeText(context,"Tidak ada yg terpilih !",Toast.LENGTH_SHORT).show();
+                }
             }
         });
         return rowView;
