62df6151ffe621b4b19b3fe992780d985bf2f2aa
Muhamad Fadlur eL-Rahman <Muhamad Fadlur eL-Rahman>
2016-06-27 22:42:11 +0700

    #About Dilo Finish

--- app/src/main/java/com/dilomalang/dilomalang/activity/AboutDiloActivity.java
+++ app/src/main/java/com/dilomalang/dilomalang/activity/AboutDiloActivity.java
@@ -1,7 +1,11 @@
 package com.dilomalang.dilomalang.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.dilomalang.dilomalang.R;
 
@@ -17,8 +21,51 @@ import com.dilomalang.dilomalang.R;
  */
 
 public class AboutDiloActivity extends AppCompatActivity {
+    Button about_twitter;
+    Button about_FB;
+    Button about_Instagram;
+    Button about_website;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dilo_page_about_dilo);
+        about_twitter = (Button) findViewById(R.id.about_twitter);
+        about_twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://twitter.com/dilomalang"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        about_FB = (Button) findViewById(R.id.about_fb);
+        about_FB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.facebook.com/DiLoMalang"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        about_Instagram = (Button) findViewById(R.id.about_instagram);
+        about_Instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.instagram.com/dilomalang/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        about_website = (Button) findViewById(R.id.about_website);
+        about_website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://malang.dilo.id/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
